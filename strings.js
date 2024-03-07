b0d72589ea88c9bf993d850c109d2ac1b8b17045
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-03-07 23:00:45 +0300

    add files

---
+++ strings.js
@@ -0,0 +1,43 @@
+//extract the last four characters from the string below.
+let text = 'extravaganza';
+console.log(text.substr(8, ));
+
+//Insert in the fourth index of the variable 1.'eat' const food = 'The quick fox jumped over the lazy dog'
+const food = "The quick fox jumped over the lazy dog";
+let subStr = "eat";
+let pos = 5;
+console.log(food.substr(0,pos) + subStr + food.substr(pos));
+
+//Count how many times the following string appers in the string variable "the"
+function stry(params) {
+    const story = 'The quick brown fox jumps over the lazy dog';
+    console.log(
+        (story.match(/the/g)).length);
+}
+stry()
+
+//Find the following words from the following strings 1.'are' const string1='The pupils are reading in the library';
+//'sitting' const='The child was sitting on the table before it fell'
+
+
+
+
+
+
+
+//Convert the following strings into the specified format
+const state = 'wonderful';
+console.log('state' {toUpperCase});
+
+const statement = 'amazing';
+console.log(statement.toLowerCase);
+
+const sententence = 'UndERneath';
+console.log(sentence.toLowerCase);
+
+ //statement = 'A wonderful world';
+ //let titleCase = '';
+ //statement.split(' ').forEach(word =>{const capitalisedWord = word.charAt(0).toUpperCase() + word.titleCase += capitalisedWord + ' '});
+ //console.log(titleCase);
+
+
